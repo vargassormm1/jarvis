4b190e9736c72c2cfdb69fa8f1467bdb6fc7d2ef
Ralph Vargas <ralphvargassorm@gmail.com>
2024-01-18 16:56:56 -0500

    added readline

--- chat.js
+++ chat.js
@@ -1,12 +1,35 @@
 import "dotenv/config";
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
 import OpenAI from "openai";
 
-const openai = new OpenAI();
-
+const rl = readline.createInterface({ input, output });
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 const chat = async () => {
+  console.log(
+    "\n-----------------------------------------------------------------------------------------------------------------------------"
+  );
+  console.log(
+    "Hello! I'm Jarvis, your personal AI chatbot. Feel free to ask me anything. If you wish to exit, just type 'exit'. Let's chat!"
+  );
+  console.log(
+    "-----------------------------------------------------------------------------------------------------------------------------\n"
+  );
+
+  const userQuestion = await rl.question("You: ");
+  if (userQuestion.toLowerCase() === "exit") {
+    rl.close();
+    return;
+  }
+
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
     model: "gpt-3.5-turbo",
+    messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      { role: "user", content: userQuestion },
+    ],
   });
 
   console.log(completion.choices[0]);
